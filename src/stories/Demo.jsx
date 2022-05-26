ff1ea5c31e39af18bccccae233e1d6b5108059da
Nelson Khiev <nelsonkhiev@gmail.com>
2022-05-26 16:43:28 -0400

    init

---
+++ src/stories/Demo.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu";
+
+import "./demo.css";
+
+export const Demo = () => {
+	return (
+		<div className="property-listing">
+			<div className="property-listing__categories">categories</div>
+			<div className="property-listing__content">content</div>
+		</div>
+	);
+};
